72e2fd12e01b1ce89ce021a5c5e4f9f2dbe42d6b
11205058 <11205058@l1847300.portoalegre.pucrsnet.br>
2017-04-03 19:29:56 -0300

    Inicial...

--- MarsRoverT168/src/mars/rover/Plateau.java
+++ MarsRoverT168/src/mars/rover/Plateau.java
@@ -2,13 +2,20 @@ package mars.rover;
 
 public class Plateau {
 
-	//inicio
-	Plateau[][] plateau = new Plateau[5][5];
+	Rover[][] plateau; 
 
 	public Plateau(int x, int y) {
-		
+		plateau = new Rover[x][y];
 	}
 	
 	
+	
+//	string.toUpperCase()
+//	string.toLowerCase()
+//	Scanner s = new Scanner(System.in);
+//	String nome = s.nextLine();
+//	for(int i = 0; i < nome.length(); i++) {
+//	  System.out.println(nome.charAt(i));
+//	}
 			
 }
--- MarsRoverT168/src/mars/rover/Rover.java
+++ MarsRoverT168/src/mars/rover/Rover.java
@@ -2,6 +2,110 @@ package mars.rover;
 
 public class Rover {
 	
-	//inicio
+	private int coordX;
+	private int coordY;
+	private char cardinalPoint;
+	
+	public Rover(char cardinalPoint) {
+		this.cardinalPoint = cardinalPoint;
+		this.coordX = 0;
+		this.coordY = 0;
+	}
+	
+	public Rover(char cardinalPoint, int x, int y) {
+		this.cardinalPoint = cardinalPoint;
+		this.coordX = x;
+		this.coordY = y;
+	}
 
+	public char getCardinalPoint() {
+		return cardinalPoint;
+	}
+
+	private void setCardinalPoint(char cardinalP) {
+		this.cardinalPoint = cardinalP;
+	}
+	
+	public int getCoordX() {
+		return coordX;
+	}
+
+	private void setCoordX(int coordX) {
+		this.coordX = coordX;
+	}
+
+	public int getCoordY() {
+		return coordY;
+	}
+
+	private void setCoordY(int coordY) {
+		this.coordY = coordY;
+	}
+
+	public void left(){
+		switch (cardinalPoint){
+		case 'N':
+			setCardinalPoint('O');
+			break;
+		case 'O':
+			setCardinalPoint('S');
+			break;
+		case 'S':
+			setCardinalPoint('L');
+			break;
+		case 'L':
+			setCardinalPoint('N');
+			break;
+		}
+	}
+	
+	public void right(){
+		switch (cardinalPoint){
+		case 'N':
+			setCardinalPoint('L');
+			break;
+		case 'L':
+			setCardinalPoint('S');
+			break;
+		case 'S':
+			setCardinalPoint('O');
+			break;
+		case 'O':
+			setCardinalPoint('N');
+			break;
+		}
+	}
+		
+	public void move(){
+		switch (cardinalPoint){
+		case 'N':
+			setCoordY(coordY+1);
+			break;
+		case 'S':
+			setCoordY(coordY-1);
+			break;		
+		case 'L':
+			setCoordX(coordX+1);
+			break;
+		case 'O':
+			setCoordX(coordX-1);
+			break;
+		}
+					
+	}
+	
+	public void command(char comm){
+		switch (comm){
+		case 'L':
+			left();
+			break;
+		case 'R':
+			right();
+			break;
+		case 'M':
+			move();
+			break;
+		}
+	}
 }
+
